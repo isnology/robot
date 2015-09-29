782aa496a3fe8602aa0d1b9406ab090948e3e941
IsNology <glenn@isnology.com>
2015-09-29 13:40:40 +1000

    latest updates

--- lib/application.rb
+++ lib/application.rb
@@ -2,8 +2,6 @@
 require_relative '../lib/table'
 require_relative '../lib/robot'
 require_relative '../lib/input'
-require_relative '../lib/process_command'
-require_relative '../lib/coordinates'
 
 class Application
 
@@ -11,14 +9,13 @@ class Application
     @table = Table.new
     @robot = Robot.new
     @input = Input.new
-    @process_cmd = ProcessCommand.new(@robot, @table)
   end
 
   def run
     loop do
       @input.enter_command
       break if @input.quit?
-      @process_cmd.execute_command(@input.command, @robot, @table)
+      @robot.__send__(@input.command[0].downcase, @input.command, @table)
     end
   end
 
